5d1ae5807f293c25f2b4482af9b42ff9030e1cf2
Nikita Bochkarev <405663260nikita@gmail.com>
2023-06-08 23:44:49 +0300

    created product views

--- myshop/urls.py
+++ myshop/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from myshop import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls'), namespace='shop'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from shop.models import Product, Category
+
+
+class ProductsList(ListView):
+    def get(self, request, category_slug=None, *args, **kwargs):
+        category = None
+        products = Product.objects.filter(available=True)
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+        categories = Category.objects.all()
+
+        return render(request,
+                      'shop/product/list.html',
+                      {'category': category,
+                       'categories': categories,
+                       'products': products})
+
+
+class ProductDetail(DetailView):
+    def get(self, request, *args, **kwargs):
+        product = get_object_or_404(Product,
+                                    id=kwargs['id'],
+                                    slug=kwargs['slug'],
+                                    available=True)
+        return render(request,
+                      'shop/product/detail.html',
+                      {'product': product})
