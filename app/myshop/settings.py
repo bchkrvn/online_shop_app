debb3a2288bc28d74a7202c27308a0a9872f58cb
Nikita Bochkarev <405663260nikita@gmail.com>
2023-07-21 16:57:30 +0300

    update celery in docker-compose.yaml, fixed settings.py. added migrations

--- app/myshop/settings.py
+++ app/myshop/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'POST': os.environ.get('DB_PORT')
     }
